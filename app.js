0b8003eb2db8c624a70353aba5e4aaac6950314b
Leon <n661437@mvrht.net>
2017-09-19 11:51:38 +0200

    added choose colors & clear screen

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var users  = []
+//var colors  = []
+var lastPositions  = []
 /*
 app.listen(8080, ()=>{
   console.log('express listening on port 8080')
@@ -22,10 +24,13 @@ app.use('/static/js/', express.static(__dirname+'/static/js/'))
 
 io.on('connection', socket =>{
   users.push(socket.id)
+  let color = Math.floor(Math.random()*15)
+  //colors.push(color)
+  lastPositions.push({x: 0, y: 0})
   //console.log(users)
   console.log('a user connected '+socket.id+', there are '+ users.length + ' now');
 
-  socket.emit('get users', users); //to new user
+  socket.emit('self connected', {users: users, color: color}); //to new user
   socket.broadcast.emit('user connect', socket.id); //to all the old users
 
   socket.on('disconnect', function(msg){
@@ -33,14 +38,28 @@ io.on('connection', socket =>{
       //delete users[socket.id]
       var index = users.indexOf(socket.id);
       users.splice(index, 1);
+      //colors.splice(index, 1);
+      lastPositions.splice(index, 1);
 
       console.log('a user disconnected, there are '+ users.length + ' now');
       io.emit('user disconnect', socket.id);
     });
 
-    socket.on('user mousemove', function(position){
-        socket.broadcast.emit('user mousemove', {position: position, userid: socket.id});
+    socket.on('user mousemove', function(data){
+      let i = users.indexOf(socket.id)
+        socket.broadcast.emit('user mousemove', {toPosition: data.position, fromPosition: lastPositions[i], color: data.clr});
+        lastPositions[i] = data.position
       });
+
+      socket.on('strokeEnd', function(position){
+        let i = users.indexOf(socket.id)
+          lastPositions[i] = undefined
+        });
+
+        socket.on('clear', function(){
+          socket.broadcast.emit('clear'); //to new user
+
+          });
 });
 
 
--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,8 @@ $(document).ready(()=>{
       lastPosition = {x: 0, y: 0},
       ctx = $('canvas')[0].getContext('2d'),
       dt = 0,
-      possibleColors = ['#001f3f','#0074D9','#7FDBFF','#39CCCC','#3D9970','#2ECC40','#01FF70','#FFDC00','#FF851B','#FF4136','#85144b','#F012BE','#B10DC9','#111111','#AAAAAA','#DDDDDD' ],
-      selfColor = Math.floor(Math.random()*possibleColors.length)
+      possibleColors = ['#111111','#001f3f','#0074D9','#7FDBFF','#39CCCC','#3D9970','#2ECC40','#01FF70','#FFDC00','#FF851B','#FF4136','#85144b','#F012BE','#B10DC9','#AAAAAA','#DDDDDD' ],
+      selfColor = 0
       ctx.lineWidth = 2
 
 $('canvas').on('mousedown', (e1)=>{
@@ -20,12 +20,29 @@ $('canvas').on('mousedown', (e1)=>{
       dt = Date.now()
         draw(ctx, {x: e.offsetX, y:e.offsetY}, lastPosition, possibleColors[ selfColor ])
         lastPosition = {x: e.offsetX, y: e.offsetY}
-        socket.emit('user mousemove', {x: e.offsetX, y: e.offsetY})
+        socket.emit('user mousemove', {position: {x: e.offsetX, y: e.offsetY}, clr: selfColor})
       }})
     })
 
     $(document).on('mouseup', (e5)=>{
         $('canvas').unbind('mousemove')
+        socket.emit('strokeEnd')
+    })
+
+    $('.colorSwatch').on('click', function(){
+      $('.active').removeClass('active')
+      selfColor = possibleColors.indexOf(this.dataset.color)
+      //alert(selfColor)
+      $(this).addClass('active')
+  })
+
+  $('.clearButton').on('click', function(){
+    ctx.clearRect(0,0,400,400)
+    socket.emit('clear')
+  })
+
+    socket.on('clear', ()=>{
+        ctx.clearRect(0,0,400,400)
     })
 
     socket.on('button click', (timestamp)=>{
@@ -34,8 +51,8 @@ $('canvas').on('mousedown', (e1)=>{
 
     socket.on('user connect', (userid)=>{
         //alert('user connect '+msg)
-        objects[userid] = {x: 0, y: 0, clr: Math.floor(Math.random()*possibleColors.length)}
-        console.log(objects)
+        //objects[userid] = {x: 0, y: 0, clr: Math.floor(Math.random()*possibleColors.length)}
+        //console.log(objects)
     })
 
     socket.on('user disconnect', (userid)=>{
@@ -47,32 +64,34 @@ $('canvas').on('mousedown', (e1)=>{
             draw(ctx, objects[key])
           }*/
     })
-    socket.on('get users', (data)=>{
-      data.forEach((userid)=>{
-        objects[userid] = {x: 0, y: 0, clr: Math.floor(Math.random()*possibleColors.length)}
+
+    socket.on('self connected', (data)=>{
+      data.users.forEach((userid)=>{
+        objects[userid] = {x: 0, y: 0}
       })
+      selfColor = data.color
+      $('.colorSwatch:nth-child('+(data.color+1)+')').addClass('active')
     })
 
     socket.on('user mousemove', (data)=>{
         //console.log(objects)
-        //console.log(data.position)
-        if(objects[data.userid]){
+        console.log(data)
+        //if(objects[data.userid]){
 
-          draw(ctx, data.position, objects[data.userid], possibleColors[ objects[data.userid].clr ])
-          objects[data.userid].x = data.position.x
-          objects[data.userid].y = data.position.y
-        }
+          draw(ctx, data.toPosition, data.fromPosition, possibleColors[ data.color ])
+          //objects[data.userid].x = data.position.x
+          //objects[data.userid].y = data.position.y
+        //}
     })
 })
 
 function draw(ctx, object, last, color){
   ctx.beginPath();
-
   if( last !== undefined && (!!last.x || !!last.y)){
     ctx.moveTo(last.x, last.y)
     ctx.lineTo(object.x, object.y)
     ctx.strokeStyle = color
-    console.log(color)
+    //console.log(color)
     ctx.stroke()
   }
 }
